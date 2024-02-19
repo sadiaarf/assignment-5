9625eff3c9c5f076ac47124e55940400d9a7cd88
sadiaarf <ummeshurobi@gmail.com>
2024-02-20 03:30:14 +0600

    added a js function

--- style/js/ticket.js
+++ style/js/ticket.js
@@ -15,6 +15,32 @@
 //    })
 // }
 
-console.log(ajdjs);
-// const allButton = document.getElementsByClassName('btn');
-// console.log(allButton);
+// btn.addEventListener('click', function onclick(){
+//     btn.style.backgroundColor = 'yellow';
+
+// })
+
+const allButton = document.getElementsByClassName('add-btn');
+let count = 8;
+
+for(const button of allButton){
+//     // e er poriborte event dewa hoise
+    button.addEventListener('click', function(event){
+        count = count - 1;
+     document.getElementById('count-container').innerText = count;
+    })
+}
+
+let increased = 0;
+for(const button of allButton){
+    // e er poriborte event dewa hoise
+    button.addEventListener('click', function(event){
+        increased = increased + 1;
+        
+        const seat = event.target.parentNode.childNodes[1].innerText;
+       
+        console.log( event.target.parentNode.childNodes[1].innerText);
+     document.getElementById("count-increased").innerText = increased;
+    })
+}
+
