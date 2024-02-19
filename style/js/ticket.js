f621219b1da1f94d1d7f5b2fc76c7f96ae6a3af3
sadiaarf <ummeshurobi@gmail.com>
2024-02-19 23:35:27 +0600

    something

---
+++ style/js/ticket.js
@@ -0,0 +1,20 @@
+// const allSeat = document.getElementsByClassName('seat-container');
+// let count = 0;
+// for (const seat of allSeat) {
+//     seat.addEventListener('click', function () {
+//         count = count - 1;
+//     })
+// }
+
+// const button = document.querySelectorAll('seat-container');
+// for (let allButtons of button){
+//    allButtons.addEventListener('click', function(){
+//     const buttons = allButtons.innerText;
+//     console.log(buttons);
+//     allButtons.setAttribute('disabled',true);
+//    })
+// }
+
+console.log(ajdjs);
+// const allButton = document.getElementsByClassName('btn');
+// console.log(allButton);
