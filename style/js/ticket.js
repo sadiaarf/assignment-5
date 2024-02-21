0608e7c47a99d021a16e082663fd4994b2e7efa2
sadiaarf <ummeshurobi@gmail.com>
2024-02-21 11:25:31 +0600

    added a js

--- style/js/ticket.js
+++ style/js/ticket.js
@@ -1,46 +1,21 @@
-// const allSeat = document.getElementsByClassName('seat-container');
-// let count = 0;
-// for (const seat of allSeat) {
-//     seat.addEventListener('click', function () {
-//         count = count - 1;
-//     })
-// }
-
-// const button = document.querySelectorAll('seat-container');
-// for (let allButtons of button){
-//    allButtons.addEventListener('click', function(){
-//     const buttons = allButtons.innerText;
-//     console.log(buttons);
-//     allButtons.setAttribute('disabled',true);
-//    })
-// }
-
-// btn.addEventListener('click', function onclick(){
-//     btn.style.backgroundColor = 'yellow';
-
-// })
 
 const allButton = document.getElementsByClassName('add-btn');
 let count = 8;
 
-for(const button of allButton){
-//     // e er poriborte event dewa hoise
-    button.addEventListener('click', function(event){
+for (const button of allButton) {
+   
+    button.addEventListener('click', function (event) {
         count = count - 1;
-     document.getElementById('count-container').innerText = count;
+        document.getElementById('count-container').innerText = count;
     })
 }
 
 let increased = 0;
-for(const button of allButton){
-    // e er poriborte event dewa hoise
-    button.addEventListener('click', function(event){
+for (const button of allButton) {
+   
+    button.addEventListener('click', function (event) {
         increased = increased + 1;
-        
-        const seat = event.target.parentNode.childNodes[1].innerText;
        
-        console.log( event.target.parentNode.childNodes[1].innerText);
-     document.getElementById("count-increased").innerText = increased;
+        document.getElementById("count-increased").innerText = increased;
     })
 }
-
